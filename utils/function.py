4eb414c577884c1b8146136f351535de34b0bd16
Andre Setiawan <andresetiawan.f@gmail.com>
2023-10-14 17:00:39 +0800

    Menyelesaikan fungsi rename

--- utils/function.py
+++ utils/function.py
@@ -1,11 +1,41 @@
 from os import path as _path, rename as _rename, listdir  # noqa: F401
-from os.path import join as _join, exists  # noqa: F401
 
+def rename(path:str, ver:bool = True):
+    ekstensi = ("mp4","mkv",  "avi","mov","wmv","flv","ogg","3gp","3p2")
 
-def rename(path:str):
     try:
-        print("Bisa coy")
-        print(listdir(path))
+        list_dir = sorted([
+            # out -> ['Otakudesu.net_YPTSBT--01_360', 'mp4']
+            [i.replace(i.split(".")[-1],"")[:-1], i.split(".")[-1]] \
+            for i in listdir(path) \
+            if i.split(".")[-1] in ekstensi
+        ])
+
+        ls_name = []
+        for i,j in enumerate(list_dir):
+            file_name = "Episode"
+            if i < 9:
+                file_name += "0"
+            #             "Episode0" +    1     + "." + mp4"
+            ls_name.append(file_name + str(i+1) + "." + j[1])
+            
+        ls_name = [i for i in zip([i[0] + "." + i[1] for i in list_dir],ls_name)]
+        print(f"Lokasi: {path} \n")
+
+        # Otakudesu.net_YPTSBT--01_360.mp4 -> Episode01.mp4
+        for before, after in ls_name:
+            print(before, after,sep=" -> ")
+
+        if ver:
+            inp = input("Rename? [yes/no]\n>>> ")
+            print(type(inp),inp)
+            if inp in ["Yes","yes","No","no"]:
+                return
+
+        # Rename File
+        for i,j in ls_name:
+            _rename(_path.join(path,i),_path.join(path,j))
+
     except FileNotFoundError as e:
         raise FileNotFoundError(f"{e}\n\nFile tidak ditemukan!\nFile Not Found!")
 
@@ -15,9 +45,6 @@ def rename(path:str):
             Can't open the folder, try checking the permissions of the folder"
         )
 
-    print("End Function")
 
 if __name__ == "__main__":
-    debug = True
-
-    rename("")
+    ...
